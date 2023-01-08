e7ac9bd17971d868e098c72d2be537c90029be9f
Paulo Soares <psoares.bj@gmail.com>
2023-01-08 23:33:28 +0000

    Fix externals declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
-import packageJson from './package.json' assert { type: 'json' };
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+const packageJson = require('./package.json');
+
 export default [
   {
+    external: ['csstype'],
     input: 'src/index.ts',
     output: [
       {
@@ -24,7 +26,7 @@ export default [
     plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()]
   },
   {
-    input: 'dist/types/index.d.ts',
+    input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()]
   }
